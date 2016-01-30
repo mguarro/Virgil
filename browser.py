e879beecaa7727fe825aaae9365731dbcc1af7ee
Nikhil Kini <nkini@ucsc.edu>
2016-01-30 15:35:55 -0800

    1. Added an index file for uploading the PDF 2. Changed routes a bit

--- browser.py
+++ browser.py
@@ -4,12 +4,18 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template('test.html')
+def index():
+    return render_template('index.html')
+
+@app.route("/split")
+def split():
+    return render_template('split.html')
     
-@app.route("/graph/")
+@app.route("/graph")
 def graph():
     return render_template('graph.html')
+
+
 
 if __name__ == "__main__":
     app.run()
