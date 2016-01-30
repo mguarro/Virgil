07a89e34aada7a3dbba5949d727338de963a725a
Nikhil Kini <nkini@ucsc.edu>
2016-01-29 21:33:05 -0800

    PDF-extract functions built into convert.py Converted-xml contains examples of what kind of text pdf-extract can extract: title, references, regions

--- convertor/convert.py
+++ convertor/convert.py
@@ -13,6 +13,7 @@ class Convertor:
         self.pdfpath = filename
         self.purename = os.path.basename(self.pdfpath).split(".")[0]
         self.textoutdir = 'converted-text/'
+        self.xmloutdir = 'converted-xml/'
 
     #TODO: Timestamps to avoid filename clashes? 
     def getTextFilePath(self):
@@ -25,9 +26,43 @@ class Convertor:
 
     #def getText(self):
     #    if os.path.exists():
-        
+ 
+    def getTitle(self):
+        self.outtext = self.xmloutdir + self.purename + "-titles" +".xml"
+        command = " ".join(["pdf-extract extract --titles",self.pdfpath,"--output",self.outtext])
+        #print(command)
+        if os.system(command) != 0:
+            print("Error in shell")
+        else:
+            print("Result stored in: ",os.path.abspath(self.outtext))
+            return os.path.abspath(self.outtext)
 
-    #def getReferenceSection(self):
+    def getReferences(self):
+        self.outtext = self.xmloutdir + self.purename + "-references" +".xml"
+        command = " ".join(["pdf-extract extract --references",self.pdfpath,"--output",self.outtext])
+        if os.system(command) != 0:
+            print("Error in shell")
+        else:
+            print("Result stored in: ",os.path.abspath(self.outtext))
+            return os.path.abspath(self.outtext)
+
+    def getResolvedReferences(self):
+        self.outtext = self.xmloutdir + self.purename + "-resolved_references" +".xml"
+        command = " ".join(["pdf-extract extract --resolved_references",self.pdfpath,"--output",self.outtext])
+        if os.system(command) != 0:
+            print("Error in shell")
+        else:
+            print("Result stored in: ",os.path.abspath(self.outtext))
+            return os.path.abspath(self.outtext)
+
+    def getRegions(self):
+        self.outtext = self.xmloutdir + self.purename + "-regions" +".xml"
+        command = " ".join(["pdf-extract extract --regions --no-lines",self.pdfpath,"--output",self.outtext])
+        if os.system(command) != 0:
+            print("Error in shell")
+        else:
+            print("Result stored in: ",os.path.abspath(self.outtext))
+            return os.path.abspath(self.outtext)        
 
 import sys
 
