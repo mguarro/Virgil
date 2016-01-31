93f23156d97137310b6c2a54878f4b3dc6b1898c
Nikhil Kini <nkini@ucsc.edu>
2016-01-30 17:05:06 -0800

    1. File upload is working 2. An interfacing file for talking to the extractor was added 3. It is just a placeholder though and needs to be changed

---
+++ DoiInterface.py
@@ -0,0 +1,8 @@
+def extract_doi(filepath):
+    return '1273218736'
+
+def search_doi(doi):
+    return 'akhdsafkdjsbf'
+
+def search_doi_by_title(doi):
+    return '124234234'
--- browser.py
+++ browser.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 from flask import render_template
+import os
+from werkzeug import secure_filename
+from DoiInterface import *
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['pdf'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        file = request.files['pdf_file']
+        if file:
+            if allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(filepath)
+                doi = extract_doi(filepath)
+                return redirect(url_for('split', doi=doi))
+        elif request.doi:
+            doi = search_doi(request.doi)
+            if doi:
+                return redirect(url_for('split', doi=doi))
+        else:
+            doi = search_doi_by_title(request.doi)
+            if doi:
+                return redirect(url_for('split', doi=doi))
     return render_template('index.html')
 
 @app.route("/split")
@@ -15,7 +39,8 @@ def split():
 def graph():
     return render_template('graph.html')
 
-
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 if __name__ == "__main__":
     app.run()
