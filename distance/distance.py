0107a11ceca60b9f134247e98d72943d8a9b6dbe
Dmitriy Rivkin <drivkin@ucsc.edu>
2016-01-31 02:01:46 -0800

    improved database_builder.py

--- distance/distance.py
+++ distance/distance.py
@@ -1,5 +1,10 @@
 import sys
 import os.path
+import keyExtract
+
+db_loc = "../database_builder/master-db/master-db.json"
+text_dir = "../database_builder/text/"
+vector_dir = "../database_builder/vectors/"
 
 def normVectorGen(keyList):
     normSum = 0
@@ -22,3 +27,30 @@ def vectorMult(normKeyList1,normKeyList2):
                 vectorResult.append((i[0],i[1]*j[1]))
 
     return vectorResult
+
+
+#figure out name of pdf from db entry, grab text for that pdf, compute vector, save vector in vectors folder
+def compute_vector_for_DOI(DOI):
+    db = TinyDB(db_loc)
+    paper = Query()
+    this_paper_dict = db.search(paper.ownDOI == DOI)[0] #returns entry as dictionary
+    name = this_paper_dict['filename']
+    ##here we call marcellos code
+    vector = keyExtract.getRakeKeywords(text_dir+name+'.txt')
+    print type(vector)
+
+#test code for compute_vector_for_DOI
+
+    #add a new column and row to similiarity matrix for the new DOI (the vector should already have been computed)
+def update_similarity_matrix(newDOI):
+    
+
+    
+    
+    
+    
+
+#add a new column and row to similiarity matrix for the new DOI (the vector should already have been computed)
+def update_similarity_matrix(newDOI):
+    
+
