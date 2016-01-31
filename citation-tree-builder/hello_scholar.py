32a9b0274f9f18e8bede6f449afeb86cf0a6b38f
Nikhil Kini <nkini@ucsc.edu>
2016-01-30 23:19:00 -0800

    Deleted stray citation-tree-builder

--- citation-tree-builder/hello_scholar.py
+++
@@ -1,1 +0,0 @@
-scholar.py -c 1 --author "albert einstein" --phrase "quantum theory"
--- citation-tree-builder/scraper.py
+++
@@ -1,5 +0,0 @@
-from lxml import html
-import requests
-
-page = requests.get('http://fmls.oxfordjournals.org/content/42/3/322.5.extract')
-tree = html.fromstring(page.content)
