6274b3993e127c4e32c42f649a6a8f95039707ed
Nikhil Kini <nkini@ucsc.edu>
2016-01-30 17:39:02 -0800

    Fixed urls to have trailing slash so that there are no not found errors

--- browser.py
+++ browser.py
@@ -10,6 +10,10 @@ ALLOWED_EXTENSIONS = set(['pdf'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+@app.route("/graph/")
+def graph():
+    return render_template('graph.html')
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -31,13 +35,10 @@ def index():
                 return redirect(url_for('split', doi=doi))
     return render_template('index.html')
 
-@app.route("/split")
+@app.route("/split/")
 def split():
     return render_template('split.html')
     
-@app.route("/graph")
-def graph():
-    return render_template('graph.html')
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
